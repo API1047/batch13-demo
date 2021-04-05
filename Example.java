9917e2446f02dee6c2c4b108625ca6d14706c379
API1047 <81964454+API1047@users.noreply.github.com>
2021-04-05 17:14:47 +0530

    Add Justice League

--- Example.java
+++ Example.java
@@ -2,5 +2,6 @@ public class Example{
 	public static void main(String[] args){
 		System.out.println("This is Great");
 		System.out.println("Hello World");
+		System.out.println("Justice League");
 	}
 }
